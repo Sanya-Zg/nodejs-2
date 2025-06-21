bca5867959a439b5ce94046b28b599de8d4fc513
Oleksandr Mas <sanya.maslovskii@gmail.com>
2025-06-21 11:41:37 +0300

    realization sorting

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,32 @@
+import product from "../models/product.js";
+import Product from "../models/product.js";
 
 export const getAllProductsStatic = async (req, res) => {
-    throw new Error('testing async errors')
-    res.status(200).json({msg: 'product testing route'})
-}
+  
+  const products = await Product.find({}).sort('-name price');
+  res.status(200).json({ products, total: products.length });
+};
 
 export const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: 'product route'})
-}
-
+  const { featured, company, name, sort } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i'};  
+  }
+  console.log(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createAt')
+  }
+  const products = await result;
+  res.status(200).json({ products, total: products.length });
+};
