f7d64ec076bd5a724021bdc6400a8f6fdeb4a9e6
Oleksandr Mas <sanya.maslovskii@gmail.com>
2025-06-21 12:23:53 +0300

    realization select and limit

--- controllers/products.js
+++ controllers/products.js
@@ -3,12 +3,12 @@ import Product from "../models/product.js";
 
 export const getAllProductsStatic = async (req, res) => {
   
-  const products = await Product.find({}).sort('-name price');
+  const products = await Product.find({}).select('name price');
   res.status(200).json({ products, total: products.length });
 };
 
 export const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
@@ -21,12 +21,24 @@ export const getAllProducts = async (req, res) => {
   }
   console.log(queryObject);
   let result = Product.find(queryObject);
+  // sort
   if (sort) {
     const sortList = sort.split(',').join(' ');
     result = result.sort(sortList);
   } else {
     result = result.sort('createAt')
   }
+
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+  }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
   res.status(200).json({ products, total: products.length });
 };
